e99d17ff818d0271858cb5c1b441cb4454d45c32
Kosuke Niwano <kosuke@mba.local>
2017-04-29 21:02:09 +0900

    回復処理を追加

--- Assets/InkPainter/Script/Player.cs
+++ Assets/InkPainter/Script/Player.cs
@@ -6,13 +6,16 @@ public class Player : MonoBehaviour {
 	public int energy;
 	public int energyMax;
 	public int playerColor;		//色(RGB32? 今はID)
-  private AudioSource audio = null;
 
 	public Satellite satellite;	//乗ってる衛星
+  private AudioSource audio = null;
+  private KeyCode paintKeyCode = KeyCode.Space;
+  private KeyCode recoveryKeyCode = KeyCode.Space;
 
 	void Start(){
-		playerColor = GameMaster.instance.AddPlayer ();
-    audio = GetComponent<AudioSource>();
+		this.playerColor = GameMaster.instance.AddPlayer ();
+    this.audio = GetComponent<AudioSource>();
+    initKey ();
 	}
 
 	public void RideSat(Satellite selectedNewSat){
@@ -20,24 +23,40 @@ public class Player : MonoBehaviour {
 		satellite = selectedNewSat;
 	}
 
+  void initKey(){
+    if (this.playerColor == 0) { // player1
+      this.paintKeyCode = KeyCode.L;
+    } else { // player2
+      this.paintKeyCode = KeyCode.A;
+    }
+
+    if (this.playerColor == 0) { // player1
+      this.recoveryKeyCode = KeyCode.P;
+    } else { // player2
+      this.recoveryKeyCode = KeyCode.Q;
+    }
+
+  }
+
 	void Update(){
 		//spaceキーでぬる
 		//time.deltaで要調整？
-		if (playerColor == 0) { // player1
-			if (energy > satellite.energyComsumption) {
-				if (Input.GetKey (KeyCode.Space)) {
-          Shout ();
-					energy -= satellite.Paint (playerColor);
-				}
-			}
-		} else { // player2
-			if (energy > satellite.energyComsumption) {
-				if (Input.GetKey (KeyCode.A)) {
-          Shout ();
-					energy -= satellite.Paint (playerColor);
-				}
-			}
-		}
+
+    if (energy > satellite.energyComsumption) {
+      if (Input.GetKey (this.paintKeyCode)) {
+        Shout ();
+        energy -= satellite.Paint (playerColor);
+      }
+    } else {
+      // TODO 鳴きたい
+    }
+
+    if (energy <= satellite.energyComsumption) {
+      if (Input.GetKey (this.recoveryKeyCode)) {
+        // TODO 鳴きたい
+        this.energy = this.energyMax;
+      }
+    }
 	}
   // 体力まんたん
   public void lifeRecovery()
@@ -59,6 +78,9 @@ public class Player : MonoBehaviour {
 		if (other.tag.Equals ("Satellite")) {
 			//ほかの衛星と当たればここにくる
 			//自分以外を排除することに注意
+      //Satellite value = other.GetComponents();
+      //
+      //value.Paint ();
 		}
 	}
 }
