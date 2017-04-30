8b3e1750e85394d666cbf07d7333264a4022eda2
YackSaw <so.bad.2.you@gmail.com>
2017-04-30 10:48:04 +0900

    キャラクター追加
    キャラクター追加

---
+++ Assets/InkPainter/Script/PlayerAnimManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimManager : MonoBehaviour {
+	Animation anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animation> ();
+		anim.Play ("FloatAnim");
+
+	}
+
+	public bool jump=false;
+	void Update(){
+		if (jump) {
+			jump = false;
+			JumpToNextSat ();
+		}
+	}
+	
+	public void JumpToNextSat(){
+		anim.Play ("Jump");
+		anim.PlayQueued ("FloatAnim");
+	}
+}
